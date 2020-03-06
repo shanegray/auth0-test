c46534271b8892c8b276df8c79a5f9d3210397c4
shanegray <shane.joseph.gray@gmail.com>
2020-03-06 14:18:14 +0000

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app hide-on-scroll color="primary" dark clipped-left>
+      <v-container>
+        <v-toolbar-title>Grady's Auth Test</v-toolbar-title>
+      </v-container>
+      <template v-slot:extension>
+        <v-tabs centered>
+          <v-tab to="/">Home</v-tab>
+          <v-tab to="/about">About</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
