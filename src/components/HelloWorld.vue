8d430ddeee3b4465c6d4b908209b924e2ebb956d
shanegray <shane.joseph.gray@gmail.com>
2020-03-06 10:23:43 +0000

    initial play around

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -91,9 +91,9 @@
         >
       </li>
       <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">
+          Twitter
+        </a>
       </li>
       <li>
         <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
@@ -142,21 +142,3 @@ export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
