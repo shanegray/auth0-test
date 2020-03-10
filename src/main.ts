2c68c7f74ba5236a9f1a413af95f311c8a066bc9
shanegray <shane.joseph.gray@gmail.com>
2020-03-10 11:39:32 +0000

    added login button with errors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+
+import { domain, clientId } from "../auth-config.json";
+import { Auth0Plugin } from "./auth";
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState: any) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 Vue.config.productionTip = false;
 
